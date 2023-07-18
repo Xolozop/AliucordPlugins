d55d4de539d97e217892a53a62556e952173ffcd
Xolozop <50333438+Xolozop@users.noreply.github.com>
2023-07-18 19:34:54 +0300

    Update MathNotation.java

--- MathNotation/src/main/java/com/aliucord/plugins/MathNotation.java
+++ MathNotation/src/main/java/com/aliucord/plugins/MathNotation.java
@@ -1,45 +1,63 @@
-package com.github.Xolozop;
+package com.github.yournamehere;
 
 import android.content.Context;
-import com.aliucord.Utils;
+import com.aliucord.CollectionUtils;
 import com.aliucord.annotations.AliucordPlugin;
-import com.aliucord.api.CommandsAPI;
+import com.aliucord.entities.MessageEmbedBuilder;
 import com.aliucord.entities.Plugin;
-import com.discord.api.commands.ApplicationCommandType;
-
-import java.util.Arrays;
+import com.aliucord.patcher.*;
+import com.aliucord.wrappers.embeds.MessageEmbedWrapper;
+import com.discord.models.user.CoreUser;
+import com.discord.stores.StoreUserTyping;
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage;
+import com.discord.widgets.chat.list.entries.ChatListEntry;
+import com.discord.widgets.chat.list.entries.MessageEntry;
 
 // Aliucord Plugin annotation. Must be present on the main class of your plugin
 @AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
 // Plugin class. Must extend Plugin and override start and stop
 // Learn more: https://github.com/Aliucord/documentation/blob/main/plugin-dev/1_introduction.md#basic-plugin-structure
-public class MathNotation extends Plugin {
+public class MyFirstPatch extends Plugin {
     @Override
-    public void start(Context context) {
-        // A bit more advanced command with arguments
-        commands.registerCommand(
-                "mathnotation",
-                "Math Notation",
-                Arrays.asList(
-                        Utils.createCommandOption(ApplicationCommandType.STRING, "text", "Which text you want to convert"),
-                        Utils.createCommandOption(ApplicationCommandType.BOOLEAN, "help", "Need any help?")
-                ),
-                ctx -> {
-                    // Check if a user argument was passed
-                    if (ctx.containsArg("help") && ctx.getBoolOrDefault("help", false)) {
-                        return new CommandsAPI.CommandResult("Help");
-                    } else {
-                        // Returns either the argument value if present, or the defaultValue ("World" in this case)
-                        var text = ctx.getString("text");
-                        return new CommandsAPI.CommandResult("Hello! ¹");
-                    }
-                }
+    public void start(Context context) throws Throwable {
+        // Patch that adds an embed with message statistics to each message
+        // Patched method is WidgetChatListAdapterItemMessage.onConfigure(int type, ChatListEntry entry)
+        patcher.patch(
+                // see https://docs.oracle.com/javase/tutorial/reflect/class/classNew.html
+                WidgetChatListAdapterItemMessage.class.getDeclaredMethod("onConfigure", int.class, ChatListEntry.class),
+                new Hook(param -> { // see https://api.xposed.info/reference/de/robv/android/xposed/XC_MethodHook.MethodHookParam.html
+                    // Obtain the second argument passed to the method, so the ChatListEntry
+                    // Because this is a Message item, it will always be a MessageEntry, so cast it to that
+                    var entry = (MessageEntry) param.args[1];
+
+                    // You need to be careful when messing with messages, because they may be loading
+                    // (user sent a message, and it is currently sending)
+                    if (entry.getMessage().isLoading()) return;
+
+                    // Now add an embed with the statistics
+
+                    // This method may be called multiple times per message, e.g. if it is edited,
+                    // so first remove existing embeds
+                    CollectionUtils.removeIf(entry.getMessage().getEmbeds(), e -> {
+                        // MessageEmbed is an obfuscated class. However, Aliucord provides wrappers for commonly used
+                        // obfuscated classes, the MessageEmbedWrapper in this case.
+                        return "Message Statistics".equals(MessageEmbedWrapper.getTitle(e));
+                    });
+
+                    // Creating embeds is a pain, so Aliucord provides a convenient builder
+                    var embed = new MessageEmbedBuilder().
+                            setTitle("Message Statistics")
+                            .addField("Length", entry.getMessage().getContent() != null ? Integer.toString(entry.getMessage().getContent().length()) : "0", false)
+                            .addField("ID", Long.toString(entry.getMessage().getId()), false).build();
+
+                    entry.getMessage().getEmbeds().add(embed);
+                })
         );
     }
 
     @Override
     public void stop(Context context) {
-        // Unregister all commands
-        commands.unregisterAll();
+        // Remove all patches
+        patcher.unpatchAll();
     }
 }
