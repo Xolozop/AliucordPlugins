c58a9119973b22c5fa434d20b30c7fa10868355a
Xolozop <ianeska33@outlook.com>
2023-07-18 14:51:01 +0300

    main

---
+++ MathNotation/src/main/java/com/aliucord/plugins/MathNotation.java
@@ -0,0 +1,45 @@
+package com.github.Xolozop;
+
+import android.content.Context;
+import com.aliucord.Utils;
+import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.api.CommandsAPI;
+import com.aliucord.entities.Plugin;
+import com.discord.api.commands.ApplicationCommandType;
+
+import java.util.Arrays;
+
+// Aliucord Plugin annotation. Must be present on the main class of your plugin
+@AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
+// Plugin class. Must extend Plugin and override start and stop
+// Learn more: https://github.com/Aliucord/documentation/blob/main/plugin-dev/1_introduction.md#basic-plugin-structure
+public class MathNotation extends Plugin {
+    @Override
+    public void start(Context context) {
+        // A bit more advanced command with arguments
+        commands.registerCommand(
+                "mathnotation",
+                "Math Notation",
+                Arrays.asList(
+                        Utils.createCommandOption(ApplicationCommandType.STRING, "text", "Which text you want to convert"),
+                        Utils.createCommandOption(ApplicationCommandType.BOOLEAN, "help", "Need any help?")
+                ),
+                ctx -> {
+                    // Check if a user argument was passed
+                    if (ctx.containsArg("help") && getBoolOrDefault("help", false)) {
+                        return new CommandsAPI.CommandResult("Help");
+                    } else {
+                        // Returns either the argument value if present, or the defaultValue ("World" in this case)
+                        var text = ctx.getString("text");
+                        return new CommandsAPI.CommandResult("Hello! ¹");
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void stop(Context context) {
+        // Unregister all commands
+        commands.unregisterAll();
+    }
+}
