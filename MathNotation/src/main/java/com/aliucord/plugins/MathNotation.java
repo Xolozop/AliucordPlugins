06d697ad5615d7cd86f7799f2d1f846f3f8c427c
Xolozop <50333438+Xolozop@users.noreply.github.com>
2023-08-04 20:29:28 +0300

    Update MathNotation.java

--- MathNotation/src/main/java/com/aliucord/plugins/MathNotation.java
+++ MathNotation/src/main/java/com/aliucord/plugins/MathNotation.java
@@ -239,11 +239,10 @@ public class MathNotation extends Plugin {
 	}
 
 	public static String reverseString(String str) {  
-			StringBuilder sb = new StringBuilder(str);  
-			sb.reverse();  
-			return sb.toString();  
-		}  
-	}
+		StringBuilder sb = new StringBuilder(str);  
+		sb.reverse();  
+		return sb.toString();  
+	} 
 
 
     @Override
